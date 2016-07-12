5375595095dbcc6b77aa73f668f84a6fb1c9056e
Krzysztof Bialecki <chrisbialecki@yahoo.co.uk>
2016-07-12 17:58:31 +0100

    go

---
+++ MVVM_Study_1/MVVM_Study_1/Model/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MVVM_Study_1.Model
+{
+    public class Student : INotifyPropertyChanged
+    {
+
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnProperyChanged("FirstName");
+                OnProperyChanged("FullName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnProperyChanged("LastName");
+                OnProperyChanged("FullName");
+            }
+        }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName;  }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+
+        void OnProperyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+
+    }
+}
---
+++ MVVM_Study_1/MVVM_Study_1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_Study_1.Model;
+
+namespace MVVM_Study_1.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+          get;
+          set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> _students = new ObservableCollection<Student>();
+            _students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            _students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            _students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = _students;
+        }
+
+
+    }
+}
