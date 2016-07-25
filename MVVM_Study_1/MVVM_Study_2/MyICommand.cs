5caa13ac51ceca46debb2d435344fd6c20b475c4
chrisbialecki <chrisbialecki@yahoo.co.uk>
2016-07-25 22:27:19 +0100

    go

---
+++ MVVM_Study_1/MVVM_Study_2/MyICommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Study_2
+{
+    public class MyICommand<T> :  ICommand
+    {
+
+        Action<T> _TargetExecuteMethod;
+        Func<T, bool> _TargetCanExecuteMethod;
+
+        public MyICommand(Action<T> executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+
+        }
+
+        public MyICommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+
+            if (_TargetCanExecuteMethod != null)
+            {
+                T tparm = (T)parameter;
+                return _TargetCanExecuteMethod(tparm);
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod((T)parameter);
+            }
+        }
+
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        
+    }
+}
