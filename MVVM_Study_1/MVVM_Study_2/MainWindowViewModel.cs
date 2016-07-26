c219d9abc3d28f5c8fba7f016736494c5d4692b4
chrisbialecki <chrisbialecki@yahoo.co.uk>
2016-07-26 07:31:16 +0100

    go

--- MVVM_Study_1/MVVM_Study_2/MainWindowViewModel.cs
+++ MVVM_Study_1/MVVM_Study_2/MainWindowViewModel.cs
@@ -17,16 +17,17 @@ namespace MVVM_Study_2
 
         private CustomerListViewModel custListViewModel = new CustomerListViewModel();
         private OrderViewModel orderViewModelModel = new OrderViewModel();
-        private BindableBase _CurrentViewModel;
+       
+       private BindableBase _CurrentViewModel;
 
         public BindableBase CurrentViewModel
         {
             get { return _CurrentViewModel; }
             set { SetProperty(ref _CurrentViewModel, value); }
-
         }
 
-        public MyICommand<string> NavCommand { get; private set; }
+        
+       public MyICommand<string> NavCommand { get; private set; }
 
 
         private void OnNav(string destination)
